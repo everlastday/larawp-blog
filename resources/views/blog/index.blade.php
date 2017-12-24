8d7f7084f282b6c45d898b638e01dd34bb7eeb0c
WestSIDE <eastside@bigmir.net>
2017-12-24 23:32:19 +0200

    Fix the blog post route url

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -17,7 +17,7 @@
                     @endif
                     <div class="post-item-body">
                         <div class="padding-10">
-                            <h2><a href="{{ route('blog.show', $post->id) }}">{{ $post->title  }}</a></h2>
+                            <h2><a href="{{ route('blog.show', $post->slug) }}">{{ $post->title  }}</a></h2>
                             <p>{{ $post->excerpt }}</p>
                         </div>
 
@@ -31,7 +31,7 @@
                                 </ul>
                             </div>
                             <div class="pull-right">
-                                <a href="{{ route('blog.show', $post->id) }}">Continue Reading &raquo;</a>
+                                <a href="{{ route('blog.show', $post->slug) }}">Continue Reading &raquo;</a>
                             </div>
                         </div>
                     </div>
